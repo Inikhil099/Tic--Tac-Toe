904da15d5b4578e12071c1c3e62e7bcae0c3cc80
nikhil <nikhilraikwar099@gmail.com>
2024-11-20 23:36:29 +0530

    compeleted

--- script.js
+++ script.js
@@ -2,159 +2,41 @@ const textdiv = document.querySelectorAll(".text");
 const result = document.querySelector(".result");
 const info = document.querySelector(".info");
 const reset = document.querySelector(".reset");
-const first = document.querySelectorAll(".text")[0];
-const second = document.querySelectorAll(".text")[1];
-const third = document.querySelectorAll(".text")[2];
-const fourth = document.querySelectorAll(".text")[3];
-const fifth = document.querySelectorAll(".text")[4];
-const sixth = document.querySelectorAll(".text")[5];
-const seventh = document.querySelectorAll(".text")[6];
-const eighth = document.querySelectorAll(".text")[7];
-const ninth = document.querySelectorAll(".text")[8];
+let arr = new Array(9).fill(null);
 
-const condition1 =
-  (first.innerHTML == "X" &&
-    second.innerHTML == "X" &&
-    third.innerHTML == "X") ||
-  (fourth.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    sixth.innerHTML == "X") ||
-  (seventh.innerHTML == "X" &&
-    eighth.innerHTML == "X" &&
-    ninth.innerHTML == "X") ||
-  (first.innerHTML == "X" &&
-    fourth.innerHTML == "X" &&
-    seventh.innerHTML == "X") ||
-  (second.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    eighth.innerHTML == "X") ||
-  (third.innerHTML == "X" &&
-    sixth.innerHTML == "X" &&
-    ninth.innerHTML == "X") ||
-  (third.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    seventh.innerHTML == "X") ||
-  (fifth.innerHTML == "X" && fifth.innerHTML == "X" && ninth.innerHTML == "X");
-
-const condition2 = (first.innerHTML == "0" &&
-  second.innerHTML == "0" &&
-  third.innerHTML == "0") ||
-(fourth.innerHTML == "0" &&
-  fifth.innerHTML == "0" &&
-  sixth.innerHTML == "0") ||
-(seventh.innerHTML == "X" &&
-  eighth.innerHTML == "0" &&
-  ninth.innerHTML == "0") ||
-(first.innerHTML == "0" &&
-  fourth.innerHTML == "0" &&
-  seventh.innerHTML == "0") ||
-(second.innerHTML == "0" &&
-  fifth.innerHTML == "0" &&
-  eighth.innerHTML == "0") ||
-(third.innerHTML == "0" &&
-  sixth.innerHTML == "0" &&
-  ninth.innerHTML == "0") ||
-(third.innerHTML == "0" &&
-  fifth.innerHTML == "0" &&
-  seventh.innerHTML == "0") ||
-(fifth.innerHTML == "0" && fifth.innerHTML == "X" && ninth.innerHTML == "0");
-  
-
-let turn = 0;
+let turn = "0";
+info.innerHTML = `${turn}'s Turn`;
 
 function write(e) {
-  if (turn == 0) {
-    e.target.innerHTML = "0";
-    turn += 1;
-    info.innerText = "X's Turn";
-    e.target.removeEventListener("click", write, false);
-    check();
-  } else if (turn == 1) {
-    e.target.innerHTML = "X";
-    turn -= 1;
-    info.innerText = "0's Turn";
-    e.target.removeEventListener("click", write, false);
-    check();
-  }
+  e.target.innerHTML = turn;
+  arr[e.target.id] = turn;
+  check();
+  turn = turn == "X" ? "0" : "X";
 }
 
 function check() {
-  if ((first.innerHTML == "X" &&
-    second.innerHTML == "X" &&
-    third.innerHTML == "X") ||
-  (fourth.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    sixth.innerHTML == "X") ||
-  (seventh.innerHTML == "X" &&
-    eighth.innerHTML == "X" &&
-    ninth.innerHTML == "X") ||
-  (first.innerHTML == "X" &&
-    fourth.innerHTML == "X" &&
-    seventh.innerHTML == "X") ||
-  (second.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    eighth.innerHTML == "X") ||
-  (third.innerHTML == "X" &&
-    sixth.innerHTML == "X" &&
-    ninth.innerHTML == "X") ||
-  (third.innerHTML == "X" &&
-    fifth.innerHTML == "X" &&
-    seventh.innerHTML == "X") ||
-  (fifth.innerHTML == "X" && fifth.innerHTML == "X" && ninth.innerHTML == "X")) {
+  if (
+    (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
+    (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
+    (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[6]) ||
+    (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
+    (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
+    (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
+    (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6])
+  ) {
     info.innerText = "Game Over !";
-    result.innerHTML = "X WON";
+    result.innerHTML = `${turn}  WON`;
     textdiv.forEach((button) => {
       button.removeEventListener("click", write, false);
     });
   }
-
   if (
-    condition1 == false &&
-    condition2 == false &&
-    first.innerHTML != "" &&
-    second.innerHTML != "" &&
-    third.innerHTML != "" &&
-    fourth.innerHTML != "" &&
-    fifth.innerHTML != "" &&
-    sixth.innerHTML != "" &&
-    seventh.innerHTML != "" &&
-    eighth.innerHTML != "" &&
-    ninth.innerHTML != ""
+    !arr.some((e) => {
+      return e == null;
+    })
   ) {
     result.innerText = "It's a TIE !";
-  } 
-  
-  
-  else if (
-    first.innerHTML == "0" &&
-    second.innerHTML == "0" &&
-    third.innerHTML == "0" ||
-    fourth.innerHTML == "0" &&
-    fifth.innerHTML == "0" &&
-    sixth.innerHTML == "0" ||
-    seventh.innerHTML == "X" &&
-    eighth.innerHTML == "0" &&
-    ninth.innerHTML == "0" ||
-    first.innerHTML == "0" &&
-    fourth.innerHTML == "0" &&
-    seventh.innerHTML == "0" ||
-    second.innerHTML == "0" &&
-    fifth.innerHTML == "0" &&
-    eighth.innerHTML == "0" ||
-    third.innerHTML == "0" &&
-    sixth.innerHTML == "0" &&
-    ninth.innerHTML == "0" ||
-    third.innerHTML == "0" &&
-    fifth.innerHTML == "0" &&
-    seventh.innerHTML == "0" ||
-    first.innerHTML == "0" && 
-    fifth.innerHTML == "X" && 
-    ninth.innerHTML == "0") {
-    info.innerText = "Game Over !";
-    result.innerHTML = "0 WON";
-    textdiv.forEach((button) => {
-      button.removeEventListener("click", write, false);
-    });
   }
 }
 
